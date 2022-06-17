f9b87ae0663565263f83165e09ed8e0cb4b85b68
DidierDefrance <didier@d-defrance.fr>
2022-06-17 11:01:35 +0200

    s

--- jeeApp/jeeAppWeb/src/main/java/tp/web/rest/CtrlRestUser.java
+++ jeeApp/jeeAppWeb/src/main/java/tp/web/rest/CtrlRestUser.java
@@ -11,6 +11,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import tp.dao.DaoUser;
 import tp.entity.User;
@@ -22,11 +24,26 @@ public class CtrlRestUser {
 	@Inject
 	private DaoUser daoUser; //déléguation vers EJB implémentant interface DaoUser
 	
+	/*
 	@GET
 	@Path("/{id}")
 	//url = http://localhost:8080/jeeAppWeb/api-user/user/1
 	public User getUserById(@PathParam("id") Integer id) {
 		return daoUser.getUserById(id); //améliorable via DTO
+	}*/
+	
+	
+	@GET
+	@Path("/{id}")
+	//url = http://localhost:8080/jeeAppWeb/api-user/user/1
+	public Response getUserById(@PathParam("id") Integer id) {
+		User u = daoUser.getUserById(id); //améliorable via DTO
+		if(u==null)
+			return Response.status(Status.NOT_FOUND).build();
+		else
+			return Response.status(Status.OK)
+			         .entity(u)
+			         .build();
 	}
 	
 	@POST
