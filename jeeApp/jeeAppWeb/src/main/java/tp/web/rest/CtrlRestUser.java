3638105f1f77b052d4bc92f6514277ef562760a3
DidierDefrance <didier@d-defrance.fr>
2022-06-17 09:34:37 +0200

    s

--- jeeApp/jeeAppWeb/src/main/java/tp/web/rest/CtrlRestUser.java
+++ jeeApp/jeeAppWeb/src/main/java/tp/web/rest/CtrlRestUser.java
@@ -1,10 +1,14 @@
 package tp.web.rest;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 import tp.dao.DaoUser;
 import tp.entity.User;
@@ -21,6 +25,20 @@ public class CtrlRestUser {
 	//url = http://localhost:8080/jeeAppWeb/api-user/user/1
 	public User getUserById(@PathParam("id") Integer id) {
 		return daoUser.getUserById(id);
+	}
+	
+	@GET
+	@Path("")
+	//url = http://localhost:8080/jeeAppWeb/api-user/user
+	//url = http://localhost:8080/jeeAppWeb/api-user/user?username=toto
+	public List<User> getUsersByCriteria(@QueryParam("username") String username) {
+		List<User> listeUsers = daoUser.getAllUser();
+		if(username==null) 
+			return listeUsers;
+		else 
+			return listeUsers.stream()
+				   .filter(u-> u.getUsername().equals(username))
+				   .collect(Collectors.toList());
 	}
 
 }
